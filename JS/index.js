8b1e26f3ce88ac4ff915467801b987e74baf57d7
Wil-Rs <43270145+Wil-Rs@users.noreply.github.com>
2020-05-26 15:00:23 -0500

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,57 @@
+let input = document.querySelector('#pesquisaInp').value
+let info = document.querySelector('#info')
+let busca = document.querySelector('#btPesquisa')
+let foto = document.querySelector('#foto')
+let proxima = document.querySelector('#proxima')
+let principal = document.querySelector('#principal')
+let muda = 0
+let vetMuda = []
+
+function mudar(){
+	muda ++
+	if(muda >= 7){
+		muda = 0
+	}
+	if( vetMuda[muda] == null ){
+		muda ++
+	}
+	foto.src = vetMuda[muda]
+}
+function loadStart(){
+	document.querySelector('#loading').style.display = 'block'
+	principal.style.display = 'none'
+}
+
+function loadStop(){
+	document.querySelector('#loading').style.display = 'none'
+	axios.isCancel()
+}
+
+function con(url){
+	axios.get(url)
+	.then(resp => {
+		info.innerHTML = `
+		<p>Nome: ${resp.data.name}</p>
+		<p>Peso: ${resp.data.weight}</p>
+		<p>Altura: ${resp.data.height}</p>
+		`
+		// foto.src = resp.data.sprites.back_female
+		Object.keys(resp.data.sprites).forEach(itens => {
+			vetMuda.push( resp.data.sprites[itens] )
+		})
+		// console.log(resp.data)
+		foto.src = vetMuda[muda]
+		principal.style.display = 'block'
+	})
+	.catch(err => alert(err))
+	.finally( loadStop() )// chama quando tudo acaba
+}
+
+function buscar(){
+	let url = `http://pokeapi.co/api/v2/pokemon/${input}`
+	loadStart()
+	setTimeout( function(){ con(url) } , 3000)
+}
+
+busca.addEventListener('click', buscar)
+proxima.addEventListener('click', mudar)
